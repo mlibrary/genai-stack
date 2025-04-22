d76a1f23eb92e651f44434f30711f1cba0d0dbf3
Albert Bertram <bertrama@umich.edu>
2025-04-22 19:18:08 -0400

    Fixing docker image build.

--- py/setup.sh
+++ py/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ ! -e /app/venv/bin/activate ] ; then
-  mkdir -p /app/venv/tmp /app/venv/cache/pip /app/lib/cache/hf
+  mkdir -p /app/venv/tmp /app/venv/cache/pip /app/venv/lib/cache/hf
   python -m venv /app/venv
   source /app/venv/bin/activate
   pip install -r requirements.txt
